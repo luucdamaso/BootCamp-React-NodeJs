11cb042b9982d7e944ad345cef6823befdb67d82
luucdamaso <lucas.s.damaso@hotmail.com>
2023-03-04 22:44:49 -0300

    Resolução dos Exercícios de Arrays

--- Exercicios/Exercicios-JS/Exercicios/arrays.js
+++ Exercicios/Exercicios-JS/Exercicios/arrays.js
@@ -2,13 +2,15 @@
 
 let numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-for(let numero of numeros) {
-    console.log(numero);
-}
+// for(let numero of numeros) {
+//     console.log(numero);
+// }
 
-for(let i = 1; i <= numeros.length; i++) {
-    console.log(i)
-}
+// for(let i = 1; i <= numeros.length; i++) {
+//     console.log(i)
+// }
+
+numeros.forEach((number) => console.log(number));
 
 //2. Crie uma função que recebe um array de strings como parâmetro e re-torne um novo array apenas com as strings acima de 5 caracteres.
 
@@ -67,23 +69,66 @@ numerosMaioresQueAMedia(garrafa);
 
 //4. Crie um array de booleans e realize um OU de todas os valores.
 
+let booleans = [true, false, true, false, true, false];
+let controle = false;
+for(let i = 0; i < booleans.length; i++) {
+  controle = controle || booleans[i];
+}
+console.log(controle);
+
 //5. Crie uma função que recebe um array com os nomes dos alunos de uma turma, e outro array que recebe a lista de presença com os nomes. In-dique quais alunos faltaram.
 
+let alunos = ['lucas', 'nai', 'danilo', 'joao', 'jessica'];
+let presenca = ['lucas', 'nai'];
 
+function listaDePresenca(presentes, faltantes) {
+  let faltantes1 = [];
+  for(let i = 0; i < presentes.length; i++){
+    let aluno = presentes[i];
+    if(!faltantes.includes(aluno) ){
+      faltantes1.push(aluno)
+    }
+  }
+  return faltantes1;
+}
+console.log(listaDePresenca(alunos, presenca));
 //6. Crie uma função que recebe um array de números e retorna um array com todos os valores elevados ao quadrado.
+
+let number = [2, 3, 8, 7, 9, 1];
+const quadrado = [];
+number.map((num) => quadrado.push(num * num));
+console.log(quadrado);
 
 //7. Crie uma função que recebe um array de nomes de arquivos, e recebe também uma extensão, a função deve retornar apenas os nomes de arquivo que forem dessa extensão.
 
+const nomeArquivos = ['nome.png', 'imagem.jpg', 'java.mp3', 'node.mp4'];
+const extensão = 'mp3';
+const arquivosExt = nomeArquivos.filter((arq => arq.endsWith(extensão)));
+console.log(arquivosExt);
+
+// function nomeExtensao(arquivos, extensao){
+//   for (let i = 0; i < arquivos.length; i++) {
+//     let arquivo = arquivos[i]
+//     if(arquivo.slice(-3) == extensao){
+//       arquivosExt.push(arquivo);
+//     }
+//   }
+//   console.log(arquivosExt);
+// }
+
+
 //8. Crie uma função que recebe um número, em seguida retorne um array de números aleatórios com o tamanho especificado pelo número.
+
+let numero = 8;
+const numbers = [];
+function numeroAleatorio(number) {
+  for (let i = 0; i < number; i++) {
+    numbers.push(Math.floor(Math.random() * 100));
+  }
+  console.log(numbers);
+}
+
+numeroAleatorio(numero);
 
 //9. Refaça os itens 1, 2, 6, e 7. Utilize as funções map, filter e forEach.
 
-let listaDeChamada = ['a', 'b', 'c', 'd', 'e'];
-let listaDePresença = ['a', 'b', 'c',];
-
-function presença (presentes, faltantes){
-  let faltantes
-  if (presentes != faltantes && presentes === faltantes ){
-    
-  }
-}
