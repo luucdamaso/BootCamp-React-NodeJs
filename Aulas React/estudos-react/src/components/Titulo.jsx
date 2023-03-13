057958f6573487bad491ae35ef783a88898d106c
luucdamaso <lucas.s.damaso@hotmail.com>
2023-03-13 16:18:13 -0300

    Estilização com CSS e React

---
+++ Aulas React/estudos-react/src/components/Titulo.jsx
@@ -0,0 +1,6 @@
+export function Titulo() {
+    const textoTitulo = "Palavras sábias do dia";
+  
+    // A parte "visual" do componente
+    return <h1>{textoTitulo}</h1>;
+  }
--- Aulas React/estudos-react/src/index.js
+++ Aulas React/estudos-react/src/index.js
@@ -1,26 +1,36 @@
 //Trazer de outro arquivo/biblioteca alguma função ou variável
 import { createRoot } from "react-dom/client";
+import { Mensagem } from "./components/Mensagem";
+import { Titulo } from "./components/Titulo";
+// Linka o arquivo css ao projeto do React
+import './styles.css';
 
 //Selecionando a div na página
 const rootElement = document.getElementById("root");
 
 // A div root será a base da aplicação
-const root = createRoot(rootElement); 
+const root = createRoot(rootElement);
 
 // Inserimos o conteúdo da página
-
-let nome = 'Almir';
-
 root.render(
-    <section>
-      <h1>React é top</h1>
-      <img width='350px' src="https://picsum.photos/200" />
-      <p>
-        Olá, tudo bem? <br /> <b>{nome.toUpperCase()}</b>
-      </p>
-      <p>Soma: {1 + 1}</p>
-    </section>
-  );
+  <div>
+    <Titulo></Titulo>
+    <Mensagem></Mensagem>
+  </div>
+);
+
+// let nome = 'Almir';
+
+// root.render(
+//     <section>
+//       <h1>React é top</h1>
+//       <img width='350px' src="https://picsum.photos/200" />
+//       <p>
+//         Olá, tudo bem? <br /> <b>{nome.toUpperCase()}</b>
+//       </p>
+//       <p>Soma: {1 + 1}</p>
+//     </section>
+//   );
 
 // JSX = JavaScript Extesion
 // No fim o HTML é convertido para JavaScript (por baixo dos panos)
