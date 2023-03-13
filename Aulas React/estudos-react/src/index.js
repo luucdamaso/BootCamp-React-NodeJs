62410a06c21601cb091058f802e344884b23ccee
luucdamaso <lucas.s.damaso@hotmail.com>
2023-03-13 11:57:52 -0300

    Aprendendo mais sobre React

---
+++ Aulas React/estudos-react/src/index.js
@@ -0,0 +1,29 @@
+//Trazer de outro arquivo/biblioteca alguma função ou variável
+import { createRoot } from "react-dom/client";
+
+//Selecionando a div na página
+const rootElement = document.getElementById("root");
+
+// A div root será a base da aplicação
+const root = createRoot(rootElement); 
+
+// Inserimos o conteúdo da página
+
+let nome = 'Almir';
+
+root.render(
+    <section>
+      <h1>React é top</h1>
+      <img width='350px' src="https://picsum.photos/200" />
+      <p>
+        Olá, tudo bem? <br /> <b>{nome.toUpperCase()}</b>
+      </p>
+      <p>Soma: {1 + 1}</p>
+    </section>
+  );
+
+// JSX = JavaScript Extesion
+// No fim o HTML é convertido para JavaScript (por baixo dos panos)
+// JSX = Açúcar Sintático
+
+// Dentro das chaves podemos escrever expressões JS!
