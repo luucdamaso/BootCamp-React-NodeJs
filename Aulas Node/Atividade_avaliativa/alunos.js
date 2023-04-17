c659f85bd75a6ca58883caa64d21cca7678ab65c
luucdamaso <lucas.s.damaso@hotmail.com>
2023-04-16 22:04:17 -0300

    att

---
+++ Aulas Node/Atividade_avaliativa/alunos.js
@@ -0,0 +1,21 @@
+const alunos = [
+    { nome: 'João', matricula: 582, media: 7.5 },
+    { nome: 'Maria', matricula: 362, media: 8.2 },
+    { nome: 'Pedro', matricula: 741, media: 6.9 },
+    { nome: 'Ana', matricula: 957, media: 9.1 },
+    { nome: 'Thiago', matricula: 203, media: 5.4 },
+    ];
+
+    function adicionarAluno(aluno) {
+        alunos.push(aluno);
+      }
+      
+      function deletarAluno(index) {
+        alunosChamada.splice(index, 1);
+      }
+      
+      function atualizarAluno(index, novoAluno) {
+        alunos[index] = novoAluno;
+      }
+
+    module.exports = {alunos, adicionarAluno, atualizarAluno, deletarAluno};
