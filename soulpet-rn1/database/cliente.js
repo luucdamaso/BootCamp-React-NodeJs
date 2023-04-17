224dbc3df0c78989b597172f1205d89d5129b81c
luucdamaso <lucas.s.damaso@hotmail.com>
2023-04-17 17:50:00 -0300

    Aula Node 17.04.23 Tarde

---
+++ soulpet-rn1/database/cliente.js
@@ -0,0 +1,31 @@
+// Modelo para gerar tabela de clientes no MySQL
+// Mapeamento: cada propriedade vira uma coluna da tabela.
+
+// DataTypes = serve para definir qual o tipo da coluna 
+const {DataTypes} = require("sequelize");
+const {connection} = require("./database");
+const Endereco = require("./endereco");
+
+const Cliente = connection.define("cliente", {
+    // Configurar a coluna 'nome'
+    nome: { // nome VARCHAR NOT NULL
+        type: DataTypes.STRING(130), 
+        allowNull: false, // NOT NULL
+    },
+    email: { // Email VARCHAR UNIQUE NOT NULL
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+    },
+    telefone: { // telefone VARCHAR NOT NULL
+        type: DataTypes.STRING,
+        allowNull: false,
+    }
+});
+
+// Associação 1:1 (One-to-One)
+
+Cliente.hasOne(Endereco); // Cliente tem um Endereço
+Endereco.belongsTo(Cliente); // Endereço pertence a um Cliente
+
+module.exports = Cliente;
--- soulpet-rn1/index.js
+++ soulpet-rn1/index.js
@@ -9,10 +9,44 @@ app.use(express.json()); // Possibilitar transitar dados usando JSON
 // Configuração do Bando de Dados
 const {connection, authenticate} = require("./database/database")
 authenticate(connection); // efetivar a conexão
-
+//Configurar o model da aplicação
+const Cliente = require("./database/cliente")
+const Endereco = require("./database/endereco")
 // Definição de rotas
+app.get("/clientes", async(req, res) => {
+   const listaClientes = await Cliente.findAll();
+   res.json(listaClientes);
+});
+
+app.get("/clientes/:id", async(req, res) => {
+    // SELECT * FROM clientes WHERE id = ;
+    const cliente = await Cliente.findOne({ where: {id: req.params.id}, include: [Endereco] });
+    if(cliente) {
+        res.json(cliente);
+    } else {
+        res.status(404).json({message: "Usuário não encontrado."});
+    }
+})
+
+app.post("/clientes", async(req, res) => {
+    const { nome, email, telefone, endereco } = req.body;
+    
+    try {
+        // Dentro de 'novo' estará o objeto criado
+        const novo = await Cliente.create(
+            { nome, email, telefone, endereco }, {include: [Endereco]}// Permite inserir cliente e endereço em um comando
+            );
+        res.status(201).json(novo);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({message: "Um erro aconteceu!"})
+    }
+});
 
 // Escuta de eventos (Listen)
 app.listen(3000, () => {
+    // Gerar as tabelas a partir do model
+    // Force = apaga tudo e recria as tabelas
+    connection.sync({force: true});
     console.log("Servidor rodando em http://localhost:3000");
 });
